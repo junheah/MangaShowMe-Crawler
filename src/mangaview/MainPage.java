96ee06bbbab94f512d3e1a38c2d6388de96a1e08
junheah <38402885+junheah@users.noreply.github.com>
2019-02-17 09:15:02 +0900

    Update MainPage.java

--- src/mangaview/MainPage.java
+++ src/mangaview/MainPage.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import ml.melun.mangaview.Preference;
-
 
 public class MainPage {
     ArrayList<Manga> recent, ranking;
