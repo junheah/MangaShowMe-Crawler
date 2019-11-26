dad391e6913797c9b83617659551782372c0ac44
junheah <junheah@gmail.com>
2019-11-27 08:08:26 +0900

    Update Main.java

--- src/mangaview/Main.java
+++ src/mangaview/Main.java
@@ -11,15 +11,11 @@ import java.net.URLConnection;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-
-import com.twelvemonkeys.imageio.plugins.jpeg.*;
 import javax.net.ssl.HttpsURLConnection;
 
 import org.json.JSONObject;
